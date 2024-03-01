e645dbdee00a11db527c29576d662c50ee3c59ba
gpaipalondica <gpaipalondica@sycamores.indstate.edu>
2024-02-29 21:55:10 -0600

    getstarted

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import img1 from '../assets/img.png'
 import '../Styles/Home.css'
 import Loader from './Loader'
@@ -15,12 +16,19 @@ function Home() {
       },800)
   })
 
+  let nav2 = useNavigate()
+
+  function navLogin(){
+    nav2('/login')
+  }
+
   return (
     <div className='home'>
       {loading? <Loader/>: 
       <>
       <h1 className='home-text'>Welcome to Spendalyzer</h1>
       <img className='home-img' src={img1} alt="" />
+      <button onClick={navLogin}>Get Started</button>
       </>
       }
     </div>
